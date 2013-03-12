8c701c86b57a9f117c7a48e9212de19f35b7f59b
Vladimir Varankin <nek.narqo+git@gmail.com>
2013-03-12 21:26:01 +0400

    Libraries path should be calculated relative to build-root

--- .bem/nodes/arch.js
+++ .bem/nodes/arch.js
@@ -1,11 +1,7 @@
 var PATH = require('path'),
-    ENV = require('bem/lib/env'),
     GLOBAL_ROOT_NAME = '__root_level_dir';
 
 // XXX: `__root_level_dir` должна быть установлена только один раз
-//ENV.getEnv(GLOBAL_ROOT_NAME) ||
-//    ENV.setEnv(GLOBAL_ROOT_NAME, PATH.dirname(__dirname));
-
 // FIXME: подумать, как обойтись без `env`
 process.env[GLOBAL_ROOT_NAME] ||
     (process.env[GLOBAL_ROOT_NAME] = PATH.dirname(__dirname));
@@ -25,14 +21,15 @@ registry.decl('Arch', {
 
         // список изветсных библиотек блоков
         var repo = environ.getConf().libraries,
-            getLibRelPath = environ.getLibRelPath;
+            relative = PATH.relative.bind(null, this.root),
+            getLibPath = environ.getLibPath;
 
         return libs.reduce(function(enabled, lib) {
 
             if(repo[lib] == null)
                 throw new Error('Library ' + lib + ' is not registered!');
 
-            enabled[getLibRelPath(lib)] = repo[lib];
+            enabled[relative(getLibPath(lib))] = repo[lib];
             return enabled;
 
         }, {});
